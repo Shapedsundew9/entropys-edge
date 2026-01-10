aaaa98f3443c5be0524282606f04d6f4ed6a8810
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2026-01-10 16:59:14 -0500

    Dev container

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script runs after the container is created.
+# The 'set -e' command ensures that the script will exit immediately if a command fails.
+set -e
+
+echo "--- Running post-create script ---"
+
+# Activating the virtual environment
+echo "Creating virtual environment..."
+python3 -m venv .venv
+.venv/bin/pip install --upgrade pip
+.venv/bin/pip install -r requirements.txt
+
